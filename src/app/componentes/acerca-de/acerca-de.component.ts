d311235a716b4214aebb2dcb614e4e61afc07808
Lumi2905 <lucialyas15@gmail.com>
2025-06-30 22:35:38 -0500

    Html y css de acerca-de

--- src/app/componentes/acerca-de/acerca-de.component.ts
+++ src/app/componentes/acerca-de/acerca-de.component.ts
@@ -1,8 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-acerca-de',
-  imports: [],
+  imports: [
+    CommonModule, 
+    RouterModule, 
+    MatToolbarModule, 
+    MatButtonModule
+  ],
   templateUrl: './acerca-de.component.html',
   styleUrl: './acerca-de.component.css'
 })
