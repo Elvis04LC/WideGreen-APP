ef926927a3b09d15fb27fc715d1f3f81a175c6d6
Lumi2905 <lucialyas15@gmail.com>
2025-07-03 08:49:04 -0500

    Agregando importaciones a contactos.ts

--- src/app/componentes/contactos/contactos.component.ts
+++ src/app/componentes/contactos/contactos.component.ts
@@ -1,8 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-contactos',
-  imports: [],
+  imports: [
+    CommonModule, 
+    RouterModule, 
+    MatToolbarModule, 
+    MatButtonModule,
+    NavbarComponent
+  ],
   templateUrl: './contactos.component.html',
   styleUrl: './contactos.component.css'
 })
