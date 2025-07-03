3a14b30acc12a85a7644e740dd451442471c61f6
Lumi2905 <lucialyas15@gmail.com>
2025-07-03 09:29:42 -0500

    Creando nuestro-equipo

---
+++ src/app/componentes/nuestro-equipo/nuestro-equipo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuestro-equipo',
+  imports: [],
+  templateUrl: './nuestro-equipo.component.html',
+  styleUrl: './nuestro-equipo.component.css'
+})
+export class NuestroEquipoComponent {
+
+}
