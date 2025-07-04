86aaa316580b774dff689c878589ec97dd953251
Lumi2905 <lucialyas15@gmail.com>
2025-07-03 22:09:30 -0500

    Navbar con css

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, MatButtonModule, RouterModule],
+  imports: [CommonModule, NavbarComponent, MatButtonModule, RouterModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
