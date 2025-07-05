5aa09960dc5f27faf6a485cff5a830402048fcb6
gabrielinfante03 <u202222228@upc.edu.pe>
2025-07-05 16:45:13 -0500

    Arreglando guard admin

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,5 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const AdminGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
@@ -12,14 +13,20 @@ export const AdminGuard: CanActivateFn = (route, state) => {
   }
   try {
     const payload = JSON.parse(atob(token.split('.')[1]));
+    const snackBar = inject(MatSnackBar);
     if (payload.role === 'ADMIN' || payload.role === 'ROLE_ADMIN') {
       return true;
+    }
+    if (payload.role == 'USER' || payload.role == 'ROLE_USER') {
+      snackBar.open('El usuario no tiene permitido ingresar a esta opción', 'Cerrar',{ duration: 5000 });
+      return router.navigate(['/home']);
+      //router.navigate(['/home']);
+      //return true;
     }
   } catch (e) {
     router.navigate(['/login']);
     return false;
   }
-
   router.navigate(['/']);
   return false;
 };
